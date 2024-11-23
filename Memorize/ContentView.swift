36ccc0f20fe2cca33e52fe3d921c90f1d577ae9f
Nolan Kyhl <nolan@kyhl.com>
2024-11-23 16:46:09 -0500

    lecture 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    var body: some View {           // Computed property: recomputed every time someone asks for this
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.green)    // some view modifiers are passed to everything inside
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {                                    // equal to ZStack(content: { ... })
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)  // shape
+                    .strokeBorder(lineWidth: 2)     // view modifier (function(view) --> view)
+                Text("👻").font(.largeTitle)        // creating instances of structs
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        }
     }
 }
 
