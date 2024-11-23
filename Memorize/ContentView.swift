feba7f7deb4f5b9f92e459c8717e71d4b7a88108
Nolan Kyhl <nolan@kyhl.com>
2024-11-23 16:05:08 -0500

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Nolan Kyhl on 11/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Nolan Kyhl on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
